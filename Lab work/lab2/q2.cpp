ffb4480e463d5c32492d2b49f1364372d45c90bf
f2025-0904-collab <f2025-0904@bnu.edu.pk>
2025-10-29 19:54:11 +0500

    first commit

---
+++ Lab work/lab2/q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float celsius,fahrenheit;
+    cout<<"enter temperature in celsius";
+    cin>>celsius;
+    fahrenheit=(celsius*9/5)+32;
+    cout<<celsius<<"degrees clesius ="<<fahrenheit<<"degrees fahrenheit="<<endl;
+     return 0;
+
+
+    
+}
