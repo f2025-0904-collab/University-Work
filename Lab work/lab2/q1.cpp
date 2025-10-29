4b9b8ed7fc62ee2739367fdbf415adceff8613f2
f2025-0904-collab <f2025-0904@bnu.edu.pk>
2025-10-29 10:15:42 +0500

    first commit

---
+++ Lab work/lab2/q1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter two numbers:";
+    cin>>num1>>num2;
+    int sum,diff,product,qot;
+    sum=num1+num2;
+    diff=num1-num2;
+    product=num1*num2;
+    qot=num1/num2;
+
+    cout<<"Sum="<<sum<<endl;
+    cout<<"Diff="<<diff<<endl;
+    cout<<"Product="<<product<<endl;
+    cout<<"Qot="<<qot<<endl;
+
+      return 0;
+
+    
+    
+}
