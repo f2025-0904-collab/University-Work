d7ff46776b3fb231af001c23521e995c0bb50e9b
f2025-0904-collab <f2025-0904@bnu.edu.pk>
2025-11-08 13:27:56 +0500

    first commit

---
+++ Lab work/lab7/task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void checkevenodd(int n){
+    if(n%2==0)
+     cout<<n<<"is even"<<endl;
+    else
+     cout<<n<<"is odd"<<endl;
+    }
+     int main(){
+      int(num);
+      cout<<"enter a number"<<endl;
+      cin>>num;
+      checkevenodd(num);
+        return 0;
+    }
+
+
+  
